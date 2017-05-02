a1b27cf8df59d3efd6b4250ac2561147a4c37961
spriadka <spriadka@github.com>
2017-05-02 14:03:48 +0200

    added tests for AtLeastNOfFilter

---
+++ src/test/java/unit/AtLeastNOfFilterTest.java
@@ -0,0 +1,75 @@
+package unit;
+
+import cz.muni.fi.pv260.productfilter.AtLeastNOfFilter;
+import cz.muni.fi.pv260.productfilter.Filter;
+import cz.muni.fi.pv260.productfilter.FilterNeverSucceeds;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AtLeastNOfFilterTest {
+
+    private AtLeastNOfFilter<String> filter;
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructNZero(){
+        filter = new AtLeastNOfFilter<>(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testConstructNLessThanZero(){
+        filter = new AtLeastNOfFilter<>(-2);
+    }
+
+    @Test(expected = FilterNeverSucceeds.class)
+    public void testConstructNGreaterThanCountOfFilters(){
+        filter = new AtLeastNOfFilter<>(Integer.MAX_VALUE);
+    }
+
+    private class FilterNonEmptyString implements Filter<String> {
+
+        @Override
+        public boolean passes(String item) {
+            return !item.isEmpty();
+        }
+    }
+
+    @Test
+    public void testConstructValid(){
+        filter = new AtLeastNOfFilter<>(1,new FilterNonEmptyString());
+        Assert.assertNotNull("Filter should be initialized",filter);
+    }
+
+    @Test
+    public void testPassesInvalidSingleFilter(){
+        filter = new AtLeastNOfFilter<>(1,new FilterNonEmptyString());
+        Assert.assertFalse("filter::passes should fail on empty string",filter.passes(""));
+    }
+
+    @Test
+    public void testPassesValidSingleFilter(){
+        filter = new AtLeastNOfFilter<>(1,new FilterNonEmptyString());
+        Assert.assertTrue("filter:passes should succeed on non-empty string",filter.passes("Hello World!"));
+    }
+
+    private class FilterStringStartsWithH implements Filter<String>{
+
+        @Override
+        public boolean passes(String item) {
+            return item.startsWith("H");
+        }
+    }
+
+    @Test
+    public void testPassesInvalidMultipleFilters(){
+        filter = new AtLeastNOfFilter<>(2,new FilterNonEmptyString(),new FilterStringStartsWithH());
+        Assert.assertFalse("filter::passes should fail because not all filter passes",filter.passes("Welcome"));
+    }
+
+    @Test
+    public void testPassesValidMultipleFilters(){
+        filter = new AtLeastNOfFilter<String>(2,new FilterNonEmptyString(),new FilterNonEmptyString(), new FilterStringStartsWithH());
+        Assert.assertTrue("filter:passes should pass because number of passed filters is atleast n",filter.passes("Hello World!"));
+    }
+}
